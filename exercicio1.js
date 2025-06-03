95caaa0ac63bfa3a9a3a12d3ebbfda8739b1ab48
paparingos <emanoelzouza@gmail.com>
2025-06-03 17:20:00 -0300

    Delete exercicio1.js

--- exercicio1.js
+++
@@ -1,9 +0,0 @@
-
-const nome = "Emanooel"
-const sobrenome = "Cardoso"
-let idade = 18
-let estudante = true 
-
-console.log("oi meu jome e ", nome, sobrenome, "eu tenho", idade, "anos", "sou estudante", true)
-
-
