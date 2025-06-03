04ad350f45192442b109642480f64d2af3e20819
paparingos <emanoelzouza@gmail.com>
2025-06-03 17:17:35 -0300

    Add files via upload

---
+++ UC2emano/index.js
@@ -0,0 +1,19 @@
+//console.log('HelloWorld!')//
+
+
+//variavel constante (const) não pode recber outro valor, pois ela é constante
+
+//let novaVariavel = 100 //let pode ter seu valor alterado//
+
+//novaVariavel = 101
+
+//console.log(novaVariavel)
+
+const nome = "Emanoel cardoso da silva "
+const idade = 65452
+const peira = "paralelepípedo" 
+
+console.log("Olá!", "Meu nome é", nome, "e eu tenho", idade , "anos e eu conheço a palavra ", peira )
+console.log("ola meu nome e", nome2 )
+
+ let nome2 = "Pernalonga curtaperna" 
