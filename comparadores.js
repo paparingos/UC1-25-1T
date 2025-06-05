0e75e8a7c778465f030faa664e52a92bf0181cd3
paparingos <emanoelzouza@gmail.com>
2025-06-05 17:20:37 -0300

    Add files via upload

---
+++ comparadores.js
@@ -0,0 +1,52 @@
+//comparadores
+//verifica se o valor e tipo são iguais 
+ // igual=== diferente!== maior maior  e igual > e>= menor e menor igual < e <=
+
+
+ const numero1 = 10;
+ const numero2 = 3;
+ 
+ //Verifica se um é igual ao outro
+     console.log(numero1 === numero2); 
+ 
+ //Verifica se um é diferente do outro
+     console.log(numero1 !== numero2); 
+ //
+ //Verifica se um é maior que o outro 
+     console.log(numero1 > numero2);
+ 
+ //Verifica se um é maior ou igual ao outro
+     console.log(7 >= 7); 
+ 
+ //Verifica se um é menor que o outro
+     console.log(numero1 < numero2); 
+ 
+ //Verifica se um é menor ou igual ao outro
+     console.log(numero1 <= numero2);
+
+verificando = (12 !== 3)    
+console.log(verificando) 
+
+//EXERCICIO
+console.log(7 >= 7); 
+
+vrfc = ('true' !== 'false')
+console.log(vrfc)
+console.log(5 === '5');
+console.log('maria' === 'marea');
+console.log(7 <= 9); 
+console.log(10 !== 10);
+console.log(5 < 3);
+console.log(2 < 1);
+exercicio2
+
+const nome = "mika"
+const ida = 18 
+const ano = 2006
+console.log(18 === 18) 
+console.log("idade:",ida,"nome:",nome,"ano de nacsimento",ano, "é maior de iade?")
+console.log("iade em 2050")
+const idadeFutura = 18 + (2050 - 2025);
+console.log(idadeFutura); // 43
+
+
---
+++ operadores arimitimeticos22.js
@@ -0,0 +1,70 @@
+//crie um codigo em  que calcule a media aritimetica de tres notas de um aluno. mostre:]
+//o valor da media:
+
+// Função para calcular a média aritmética de três notas
+const calcularMedia = (nota1, nota2, nota3) => (nota1 + nota2 + nota3) / 3
+const nota1 = 8.5
+const nota2 = 7.0
+const nota3 = 9.2
+const mediaFinal = calcularMedia(nota1, nota2, nota3);
+console.log("a media é",mediaFinal) 
+
+//joão
+const calcularmediajão = (nota4, nota5, nota6) => (nota4 + nota5 + nota6) / 3
+const nota4 = 2.5
+const nota5 = 2.4
+const nota6 = 3.6
+
+const mediajoãofinal = calcularmediajão (nota4, nota5, nota6)
+console.log("a media joão é", mediajoãofinal) 
+
+
+
+//media robersvaoldo
+const mediarobercalculo = (nota7, nota8, nota9,) => (nota7 + nota8 + nota9) /3
+const nota7 = 7.5
+const nota8 = 4.8
+const nota9 = 6.3
+const mediarobert = mediarobercalculo (nota7, nota8, nota9,)
+console.log("media de robervaldo", mediarobert)
+
+//media maria
+const mediamariacalculo = (nota10, nota11, nota12,) => (nota10 + nota11 + nota12) /3
+const nota10 = 3.5
+const nota11 = 8
+const nota12 = 6
+const mediarmaria = mediamariacalculo (nota10, nota11, nota12,)
+console.log("media de maria", mediarmaria)
+
+//media julian 
+const mediajuliacalculo = (nota13, nota14, nota15,) => (nota13 + nota14 + nota15) /3
+const nota13 = 5
+const nota14 = 8
+const nota15 = 8
+const mediarjulia = mediajuliacalculo (nota13, nota14, nota15,)
+console.log("media de julia", mediarjulia)
+
+//maike media
+const mediamikecalculo = (nota16, nota17, nota18,) => (nota16 + nota17 + nota18) /3
+const nota16 = 8.5
+const nota17 = 7
+const nota18 = 6.5
+const mediarmike = mediamikecalculo (nota16, nota17, nota18,)
+console.log("media de mike", mediarmike)
+
+//iade em canina
+
+const idade = 18
+const idadeCanina = 18*17
+
+ console.log("eu cachjorro tenho", idadeCanina)
+
+
+
+
+
+
+
+
+
+
