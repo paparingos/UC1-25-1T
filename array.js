79c135aeff1543d526303c36e9b6985452122d93
paparingos <emanoelzouza@gmail.com>
2025-06-10 17:25:25 -0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,9 @@
+//atividade
+let racasDeCachorros = ["Labrador", "Poodle", "Bulldog", "Beagle", "Pastor Alemão"];
+
+console.log("0. " + racasDeCachorros[0]);
+console.log("1. " + racasDeCachorros[1]);
+console.log("2. " + racasDeCachorros[2]);
+console.log("3. " + racasDeCachorros[3]);
+console.log("4. " + racasDeCachorros[4]);
+
---
+++ stringearays1.js
@@ -0,0 +1,43 @@
+const nome = "mika"
+const idade = 27 
+const frase = "meu nome é " + nome + " e tenho " + idade + " anos"
+console.log(frase)
+//2
+let nome1 = "pedro"
+let idade1 = 12
+let frase2 = `meu nome é ${nome1} e tenho ${idade1} anos`
+console.log(frase2)
+//exercicio1
+let nome2 = "mig"
+let corfav = "verde" 
+let filme = "se7ven"
+let time = "flamengo" 
+let frase3 = `meu nome é ${nome2} e minha cor favorita é ${corfav} gosto muito de estudra no senac e curto o filme ${filme} alem disso meu time é o ${time}`
+console.log(frase3) 
+
+let nome3 = "miguel"
+let corfavo = "verdex" 
+let filmes = "se7vens"
+let times = "flamengos" 
+let frase4 = "meu nome é" + nome3 + "e minha cor favorita é " + corfavo + " gosto muito de estudra no senac e curto o filme " + filmes + "alem disso meu time é o " + times + "a"
+console.log(frase4)
+
+const nom = "Alvaro luiz gonzaga de souza junior"
+let comidafavorita = "lasanha de camarão"
+console.log(nom.length)
+console.log(comidafavorita.length)
+ const names = " mdslll ll " 
+ console.log(names.trim())
+
+ const frase8 = "eu comi cenoura"
+ frase.includes("cenoura")//true
+ frase.includes("ovo")//false
+ console.log( frase8.includes("ovo"), frase8.includes("cenoura"))
+
+ const frase9 = "hoje comi cenoura, adoro cenoura"
+const novafrase = frase9.replaceAll("ceneoura","batata")
+console.log(novafrase)
+
+//4//
+
+
